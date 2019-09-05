1c29fc5d3950585c90ecfed96cb3f500f97e2782
David Caldwell <david@avvitt.com>
2019-09-05 18:18:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,103 +1,21 @@
 def introduction(name)
-  puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}"
 end
 
 introduction("Josh")
 introduction("Dan")
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-language = "Ruby"
-introduction_with_language("Dan",language)
+
+introduction_with_language("Dan","Ember.js")
 introduction_with_language("Josh","React")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+def introduction_with_language_optional(name,language)
+  pputs "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+language = "Ruby"
+introduction_with_language("Edwin",language)
+introduction_with_language("Josh","Python")
