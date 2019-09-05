f1166132c5dca11e931cc86f29ffe2af0dad0d1e
David Caldwell <david@avvitt.com>
2019-09-05 18:22:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,8 +16,9 @@ introduction_with_language("Josh","React")
 def introduction_with_language_optional(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_language_optional("Edwin","Python")
-introduction_with_language_optional("Josh","Python")
+language = "Ruby"
+introduction_with_language("Edwin","Python")
+introduction_with_language("Josh",language)
 
 
 
