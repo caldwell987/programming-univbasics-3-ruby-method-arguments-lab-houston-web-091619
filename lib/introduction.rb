50289b61094584a50829472643445ccebc2107d8
David Caldwell <david@avvitt.com>
2019-09-05 18:13:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,8 +8,8 @@ introduction("Dan")
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction_with_language("Dan","Ruby")
+language = Ruby
+introduction_with_language("Dan",language)
 introduction_with_language("Josh","React")
 
 
