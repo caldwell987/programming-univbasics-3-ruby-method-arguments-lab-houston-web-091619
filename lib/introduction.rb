c63c24f0b919b636b34851627e6163b2d5bf4f75
David Caldwell <david@avvitt.com>
2019-09-05 18:05:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
-introduction("Josh.")
-introduction("Dan.")
+introduction("Josh")
+introduction("Dan")
 
 
 
